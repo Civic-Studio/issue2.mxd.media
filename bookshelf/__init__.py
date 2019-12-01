568be682fb919c9fe9f41369ad1feefcda0e61fa
Eriks Reks <eriks@erikss-air.attlocal.net>
2019-12-01 11:47:39 -0600

    Organization

--- bookshelf/__init__.py
+++ bookshelf/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, url_for, request
 from bookshelf.app.controllers import main
 
 app = Flask(__name__)
--- bookshelf/app/controllers.py
+++ bookshelf/app/controllers.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, render_template
 import os, json
 
-main = Blueprint('main', __name__, template_folder='../../templates')
+main = Blueprint('main', __name__, template_folder='../../templates', static_folder='../../static')
 
 @main.route('/')
 def show_index():
@@ -9,26 +9,17 @@ def show_index():
 	home_page = json.load(open(current_path + '/json/home.json'))
 	return render_template('index.html', home_page=home_page)
 
-#####################
-#  ESSAYS AVAILABLE #
-#####################
-# Pump Station Operators - Individuals by Aron Chang
-# Pump Station Operators - Teamwork & Coordination by Aron Chang
-# What Does a Pump Station Do?
-# Keeping the Pumps Running
-# Watch: Day After A Storm
-# Watch: Evening at Station D
-# Challenges
-# Water Creature by Carole Alden
-#####################
-
 @main.route('/essay/<title>')
 def show_essays(title=None):
 	return render_template('essay.html', title=title)
 
-@main.route('/galleries/<series>')
+@main.route('/gallery/<series>')
 def show_galleries(series=None):
 	return render_template('galleries.html', series=series)
+
+@main.route('/about')
+def show_about():
+    return render_template('about.html')
 
 @main.route('/artists')
 def show_artists():
